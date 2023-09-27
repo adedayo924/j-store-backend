d24308fbb9a6aeab169994769c5ced40f183758f
Kolade F. Opeyemi <adedayo@gmail.com>
2023-09-27 21:14:04 +0100

    Configure app with user parameters

--- core/backend/admin.py
+++ core/backend/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
-# Register your models here.
+from backend.models import User
+
+
+@register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'phone', 'fullname', 'created_at']
+    readonly_fields = ['password']
+
--- core/backend/models.py
+++ core/backend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+    fullname = models.CharField(max_length=100)
+    password = models.CharField(max_length=5000)
+    created_at = models.DateTimeField(auto_now_add=True)
